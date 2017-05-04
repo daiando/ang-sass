1e906b9483a46c72e57dc0e072237c3b9eb7b8e6
DAI ANDO <daifloss@gmail.com>
2017-05-05 03:43:08 +0900

    devide into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftsideComponent } from './leftside/leftside.component';
+import { RightsideComponent } from './rightside/rightside.component';
+import { HomeComponent } from './home/home.component';
 
+import { AppRoutingModule } from "./app-routing.module";
+import { ContentComponent } from './home/content/content.component';
+import { TeasersComponent } from './home/teasers/teasers.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftsideComponent,
+    RightsideComponent,
+    HomeComponent,
+    ContentComponent,
+    TeasersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/teasers/teasers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teasers',
+  templateUrl: './teasers.component.html',
+  styleUrls: ['./teasers.component.scss']
+})
+export class TeasersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
